22a2207fcb777daa6e5ddef6aa6b3e316cbf72c0
Anjali-Thota027 <anju.thota99@gmail.com>
2025-08-11 20:17:18 +0530

    Add files via upload

---
+++ assessment.java
@@ -0,0 +1,14 @@
+package day3;
+import java.util.Scanner;
+
+public class assessment {
+	
+	
+	 public static void main(String[] args) {
+	 Scanner sc = new Scanner(System.in);
+	 System.out.print("Enter a number: ");
+	 int a = sc.nextInt();
+	 System.out.println("One's Complement: " + (~a));
+	 sc.close();
+	 }
+	}
---
+++ calculator.java
@@ -0,0 +1,11 @@
+package day3;
+
+public class calculator {
+	public int add(int x ,int y) {
+		return x+y;
+	}
+	public int sub(int x ,int y) {
+		return x-y;
+	}
+
+}
